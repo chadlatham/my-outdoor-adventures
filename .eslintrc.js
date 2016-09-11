bfc870494de16bdbfdcd91bb4318f15cea77bca1
Chad Latham <chadlatham33@gmail.com>
2016-09-11 15:19:00 -0700

    Add linting and update .gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: [
+    'ryansobol/es6',
+    'ryansobol/node',
+  ],
+  rules: {
+    // enable additional rules
+    // "indent": ["error", 4],
+    // "linebreak-style": ["error", "unix"],
+    // "quotes": ["error", "double"],
+    // "semi": ["error", "always"],
+
+    // override default options for rules from base configurations
+    // "comma-dangle": ["error", "always"],
+    // "no-cond-assign": ["error", "always"],
+
+    // disable rules from base configurations
+    // "camelcase": "off"
+  }
+};
