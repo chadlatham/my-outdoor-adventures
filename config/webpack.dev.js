adb3d66b59bcedbf98a4ff3f04e83fc3d0f6dd9e
Chad Latham <chadlatham33@gmail.com>
2016-09-13 11:58:08 -0700

    Cleanup webpack-dev-server and configure nodemon

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,17 +1,13 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var helpers = require('./helpers');
 var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var webpackStats = require('webpack-stats-plugin').StatsWriterPlugin;
 
 module.exports = {
   entry: {
     'polyfills': './src/polyfills.ts',
     'vendor': './src/vendor.ts',
     'app': './src/main.ts'
-    // 'polyfills': ['./src/polyfills.ts', 'webpack/hot/dev-server', 'webpack-hot-middleware/client?http://0.0.0.0:3000'],
-    // 'vendor': ['./src/vendor.ts', 'webpack/hot/dev-server', 'webpack-hot-middleware/client?http://0.0.0.0:3000'],
-    // 'app': ['./src/main.ts', 'webpack/hot/dev-server', 'webpack-hot-middleware/client?http://0.0.0.0:3000']
   },
 
   resolve: {
@@ -49,9 +45,6 @@ module.exports = {
 
   output: {
     path: '/',
-    // path: helpers.root('dist'),
-    // publicPath: 'http://localhost:8080/',
-    // publicPath: 'http://localhost:3000/dist/',
     publicPath: 'http://localhost:3000/',
     filename: '[name].js',
     chunkFilename: '[id].chunk.js'
@@ -63,14 +56,5 @@ module.exports = {
     new webpack.optimize.CommonsChunkPlugin({
       name: ['app', 'vendor', 'polyfills']
     }),
-    // new webpack.optimize.OccurenceOrderPlugin(),
-    // new webpack.HotModuleReplacementPlugin(),
-    // new webpack.NoErrorsPlugin()
-    // new webpack.ResolverPlugin(new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('bower.json', ['main'], new webpackStats('webpack.json')))
   ]
-
-  // devServer: {
-  //   historyApiFallback: true,
-  //   stats: 'minimal'
-  // }
 };
