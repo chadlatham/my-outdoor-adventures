67f6f8c36c350b69b7a6014144a9f88247933300
Chad Latham <chadlatham33@gmail.com>
2016-09-14 10:10:20 -0700

    Add dedupe plugin to webpack

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -22,7 +22,7 @@ module.exports = webpackMerge(commonConfig, {
 
   plugins: [
     new webpack.NoErrorsPlugin(),
-    // new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.DedupePlugin(),
     // new webpack.optimize.UglifyJsPlugin({ // https://github.com/angular/angular/issues/10618
     //   mangle: {
     //     keep_fnames: true
